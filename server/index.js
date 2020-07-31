e19571c060d2d9940c31ab31f57905b3c45af015
Oscar Arenas <oscarenas@gmail.com>
2020-07-30 19:51:12 -0500

    ML Search App

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/api/greeting", (req, res) => {
+  const name = req.query.name || "World";
+  console.log(name);
+
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.listen(3001, () =>
+  console.log("Express server is running on localhost:3001 🟢 ")
+);
